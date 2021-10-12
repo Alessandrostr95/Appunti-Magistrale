c4cd1883ea0850962c3355c2d5041a897d61f557
Alessandro <54322448+Alessandrostr95@users.noreply.github.com>
2021-10-13 01:08:03 +0200

    Add files via upload

---
+++ publish.el
@@ -0,0 +1,26 @@
+(require 'ox-publish)
+(setq org-publish-project-alist
+      '(("appunti"
+	 :base-directory "./org/"
+	 :base-extension "org"
+	 :publishing-directory "./public_html/"
+	 :recursive t
+	 :publishing-function org-html-publish-to-html
+	 :headline-levels 4             ; default for this project.
+	 :auto-preamble t
+	 :sitemap-filename "index.org"
+	 :sitemap-title "Appunti Magistrale"
+	 :sitemap-style list
+	 
+	 )
+	("static"
+	 :base-directory "./assets/"
+	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3"
+	 :publishing-directory "./public_html/"
+	 :recursive t
+	 :publishing-function org-publish-attachment
+	 )
+       ("all" :components ("appunti" "static"))
+
+	)
+      )
